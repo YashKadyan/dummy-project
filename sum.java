ab346e75227c76385a5cec9fc109f56c9c80e5ad
YashK <yashkadyan17@gmail.com>
2023-07-14 16:00:30 +0530

    sum.java in branch b1 modified!

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum
 {
         public static void main()
         {
-                System.out.print("In sum.java file");sddmsad
+                System.out.print("In sum.java file");
         }
 }
